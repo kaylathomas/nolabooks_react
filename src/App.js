dbff2ff50efb681ada957d6f262478a0d361e48e
KayLa <35053868+kaylathomas@users.noreply.github.com>
2020-08-21 03:21:57 -0400

    App setup is complete.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
